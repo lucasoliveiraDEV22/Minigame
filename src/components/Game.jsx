cce3bbc3f036f4ecbe6c353df36c2c150ec70682
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-06-13 18:11:36 -0300

    commit new

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Result from './Result';
 import Sequence from './Sequence';
 import Timer from './Timer';
--- src/components/Sequence.jsx
+++ src/components/Sequence.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+
 
 const Sequence = ({ sequence, currentIndex }) => {
   return (
